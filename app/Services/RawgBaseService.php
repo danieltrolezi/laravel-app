6189dc267425790a5d47366f53652a90b13a01e9
Daniel Trolezi <danieltrolezi@outlook.com>
2024-06-25 16:23:15 -0300

    Cache RAWG API's response on Redis

--- app/Services/RawgBaseService.php
+++ app/Services/RawgBaseService.php
@@ -3,9 +3,12 @@
 namespace App\Services;
 
 use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Redis;
 
 abstract class RawgBaseService
 {
+    private const int CACHE_TTL = 24 * 60 * 60;
+
     protected string $apiKey;
     protected string $apiHost;
     protected Client $client;
@@ -32,15 +35,56 @@ abstract class RawgBaseService
         string $uri = '',
         array $data = ['query' => []]
     ): array {
-        $res = $this->client->request($method, $uri, [
-            'query' => array_merge($data['query'], [
-                'key' => $this->apiKey
-            ])
-        ]);
+        $contents = $this->getCacheContents($uri, $data);
 
-        return json_decode(
-            $res->getBody()->getContents(),
-            true
+        if (empty($contents)) {
+            $res = $this->client->request($method, $uri, [
+                'query' => array_merge($data['query'], [
+                    'key' => $this->apiKey
+                ])
+            ]);
+
+            $contents = $res->getBody()->getContents();
+            $this->setCacheContents($uri, $data, $contents);
+        }
+
+        return json_decode($contents, true);
+    }
+
+    /**
+     * @param string $uri
+     * @param array $data
+     * @return null|string
+     */
+    private function getCacheContents(string $uri, array $data): ?string
+    {
+        $key = $this->getCacheKey($uri, $data['query']);
+        return Redis::get($key);
+    }
+
+    /**
+     * @param string $uri
+     * @param array $data
+     * @param string $payload
+     * @return void
+     */
+    private function setCacheContents(string $uri, array $data, string $contents): void
+    {
+        $key = $this->getCacheKey($uri, $data['query']);
+        Redis::setEx($key, self::CACHE_TTL, $contents);
+    }
+
+    /**
+     * @param string $uri
+     * @param array $data
+     * @return string
+     */
+    private function getCacheKey(string $uri, array $query): string
+    {
+        return sprintf(
+            'rawg_%s_%s_',
+            $uri,
+            implode('_', $query)
         );
     }
 }
