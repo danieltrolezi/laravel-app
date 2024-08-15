d4f417454dfd2e0f0d8f005d12a41c1ced9c9f7c
Daniel Trolezi <danieltrolezi@outlook.com>
2024-08-14 22:49:47 -0300

    Implement API health route

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,11 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Spatie\Health\Checks\Checks\DatabaseCheck;
+use Spatie\Health\Checks\Checks\EnvironmentCheck;
+use Spatie\Health\Checks\Checks\RedisCheck;
+use Spatie\Health\Checks\Checks\UsedDiskSpaceCheck;
+use Spatie\Health\Facades\Health;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +24,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $env = match (config('app.url')) {
+            'http://laravel-app.local' => 'local',
+            default => 'production'
+        };
+
+        Health::checks([
+            EnvironmentCheck::new()->expectEnvironment($env),
+            UsedDiskSpaceCheck::new(),
+            DatabaseCheck::new(),
+            RedisCheck::new()
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -8,9 +8,11 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\RawgGamesController;
 use App\Http\Controllers\RawgDomainController;
 use Illuminate\Support\Facades\Route;
+use Spatie\Health\Http\Controllers\HealthCheckJsonResultsController;
 
 Route::permanentRedirect('/docs', '/swagger/index.html');
 
+Route::get('/health', HealthCheckJsonResultsController::class);
 Route::post('/account/register', [AccountController::class, 'register']);
 Route::post('/auth/login', [AuthController::class, 'login']);
 
