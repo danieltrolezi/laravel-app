c4c4f3f11e698f32b03cd34f1d3bf3cd98835a9e
Daniel Trolezi <danieltrolezi@outlook.com>
2024-09-12 14:19:57 -0300

    Prep for prod

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-if [ ! -f ./composer.lock ]; then
-    composer install --no-interaction
-    composer dump-autoload
+if [ ! -d "vendor" ] || [ -z "$(ls -A vendor)" ]; then
+    if [ "$APP_ENV" == "local" ]; then
+        composer install --no-interaction
+        composer dump-autoload
+    else
+        composer install --no-interaction --optimize-autoloader --no-dev      
+    fi
 fi
 
 /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
