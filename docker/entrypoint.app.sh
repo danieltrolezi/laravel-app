2b96635fded4bde4aab0d9ac96cc5b4bb683b7a6
Daniel Trolezi <danieltrolezi@outlook.com>
2024-09-22 00:08:12 -0300

    Only run migrations when local

--- docker/entrypoint.app.sh
+++ docker/entrypoint.app.sh
@@ -6,11 +6,10 @@ if [ ! -d "vendor" ] || [ -z "$(ls -A vendor)" ]; then
         composer dump-autoload
 
         php artisan key:generate --ansi
+        php artisan migrate --seed
     else
         composer install --no-interaction --optimize-autoloader --no-dev      
-    fi
-
-    php artisan migrate --seed
+    fi  
 fi
 
 supervisord -n -c /etc/supervisor/supervisord.conf
